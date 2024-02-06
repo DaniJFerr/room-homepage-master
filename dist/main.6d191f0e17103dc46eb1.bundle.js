413366165b8083494f1f13071e9d932d04acdb3f
DaniJFerreira <daniel.jorge.ferreira1984@gmail.com>
2024-02-06 11:22:45 +0000

    install and config npm packages, webpack instances

---
+++ dist/main.6d191f0e17103dc46eb1.bundle.js
@@ -0,0 +1,232 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/template.html":
+/*!***************************!*\
+  !*** ./src/template.html ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/html-loader/dist/runtime/getUrl.js */ \"./node_modules/html-loader/dist/runtime/getUrl.js\");\n/* harmony import */ var _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0__);\n// Imports\n\nvar ___HTML_LOADER_IMPORT_0___ = new URL(/* asset import */ __webpack_require__(/*! ../assets/logo.svg */ \"./assets/logo.svg\"), __webpack_require__.b);\nvar ___HTML_LOADER_IMPORT_1___ = new URL(/* asset import */ __webpack_require__(/*! ../assets/icon-arrow.svg */ \"./assets/icon-arrow.svg\"), __webpack_require__.b);\nvar ___HTML_LOADER_IMPORT_2___ = new URL(/* asset import */ __webpack_require__(/*! ../assets/icon-angle-left.svg */ \"./assets/icon-angle-left.svg\"), __webpack_require__.b);\nvar ___HTML_LOADER_IMPORT_3___ = new URL(/* asset import */ __webpack_require__(/*! ../assets/icon-angle-right.svg */ \"./assets/icon-angle-right.svg\"), __webpack_require__.b);\nvar ___HTML_LOADER_IMPORT_4___ = new URL(/* asset import */ __webpack_require__(/*! ../assets/image-about-dark.jpg */ \"./assets/image-about-dark.jpg\"), __webpack_require__.b);\nvar ___HTML_LOADER_IMPORT_5___ = new URL(/* asset import */ __webpack_require__(/*! ../assets/image-about-light.jpg */ \"./assets/image-about-light.jpg\"), __webpack_require__.b);\n// Module\nvar ___HTML_LOADER_REPLACEMENT_0___ = _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0___default()(___HTML_LOADER_IMPORT_0___);\nvar ___HTML_LOADER_REPLACEMENT_1___ = _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0___default()(___HTML_LOADER_IMPORT_1___);\nvar ___HTML_LOADER_REPLACEMENT_2___ = _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0___default()(___HTML_LOADER_IMPORT_2___);\nvar ___HTML_LOADER_REPLACEMENT_3___ = _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0___default()(___HTML_LOADER_IMPORT_3___);\nvar ___HTML_LOADER_REPLACEMENT_4___ = _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0___default()(___HTML_LOADER_IMPORT_4___);\nvar ___HTML_LOADER_REPLACEMENT_5___ = _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0___default()(___HTML_LOADER_IMPORT_5___);\nvar code = \"<!DOCTYPE html>\\r\\n<html lang=\\\"en\\\">\\r\\n  <head>\\r\\n    <meta charset=\\\"UTF-8\\\" />\\r\\n    <meta name=\\\"viewport\\\" content=\\\"width=device-width, initial-scale=1.0\\\" />\\r\\n    <!-- displays site properly based on user's device -->\\r\\n    <title>Frontend Mentor | Room homepage</title>\\r\\n    <!-- For example: -->\\r\\n  </head>\\r\\n\\r\\n  <body>\\r\\n    <div class=\\\"container\\\">\\r\\n      <div role=\\\"banner\\\" class=\\\"main-header\\\">\\r\\n        <div class=\\\"div\\\">\\r\\n            <header class=\\\"header-columns\\\">\\r\\n              <div role=\\\"navigation\\\" class=\\\"column\\\">\\r\\n                    <nav class=\\\"navbar\\\" aria-label=\\\"Main navigation\\\">\\r\\n                      <img\\r\\n                      loading=\\\"lazy\\\"\\r\\n                      src=\\\"\" + ___HTML_LOADER_REPLACEMENT_0___ + \"\\\"\\r\\n                      class=\\\"logo-img\\\"\\r\\n                      alt=\\\"Company Logo\\\"\\r\\n                    />\\r\\n                      <ul class=\\\"navbar-list\\\">\\r\\n                        <li class=\\\"navbar-item\\\">\\r\\n                          <a href=\\\"#\\\" class=\\\"navbar-link\\\">home</a>\\r\\n                        </li>\\r\\n                        <li class=\\\"navbar-item\\\">\\r\\n                          <a href=\\\"#\\\" class=\\\"navbar-link\\\">shop</a>\\r\\n                        </li>\\r\\n                        <li class=\\\"navbar-item\\\">\\r\\n                          <a href=\\\"#\\\" class=\\\"navbar-link\\\">about</a>\\r\\n                        </li>\\r\\n                        <li class=\\\"navbar-item\\\">\\r\\n                          <a href=\\\"#\\\" class=\\\"navbar-link\\\">contact</a>\\r\\n                        </li>\\r\\n                      </ul>\\r\\n                    </nav>\\r\\n                  </div>\\r\\n            </header>\\r\\n\\r\\n          <main class=\\\"main-content\\\">\\r\\n            <section aria-label=\\\"shopTitle\\\" class=\\\"shop-content\\\">\\r\\n              <div class=\\\"text-content\\\">\\r\\n                <h1 id=\\\"shopTitle\\\" class=\\\"shop-title\\\">Discover innovative ways to decorate</h1>\\r\\n                <p class=\\\"shop-content\\\">\\r\\n                  We provide unmatched quality, comfort, and style for property\\r\\n                  owners across the country. Our experts combine form and\\r\\n                  function in bringing your vision to life. Create a room in\\r\\n                  your own style with our collection and make your property a\\r\\n                  reflection of you and what you love.\\r\\n                </p>\\r\\n                <div class=\\\"shop-button\\\">\\r\\n                  <div class=\\\"div-8\\\" aria-label=\\\"Shop now\\\">SHOP NOW</div>\\r\\n                  <img\\r\\n                    loading=\\\"lazy\\\"\\r\\n                    src=\\\"\" + ___HTML_LOADER_REPLACEMENT_1___ + \"\\\"\\r\\n                    class=\\\"button-arrow-img\\\"\\r\\n                    alt=\\\"Arrow pointing right\\\"\\r\\n                  />\\r\\n                </div>\\r\\n              </div>\\r\\n              <div class=\\\"button-select\\\">\\r\\n                <img\\r\\n                  loading=\\\"lazy\\\"\\r\\n                  src=\\\"\" + ___HTML_LOADER_REPLACEMENT_2___ + \"\\\"\\r\\n                  class=\\\"icon-angle-left\\\"\\r\\n                  alt=\\\"button pointing left\\\"\\r\\n                />\\r\\n                <img\\r\\n                  loading=\\\"lazy\\\"\\r\\n                  src=\\\"\" + ___HTML_LOADER_REPLACEMENT_3___ + \"\\\"\\r\\n                  class=\\\"icon-angle-right\\\"\\r\\n                  alt=\\\"button pointing right\\\"\\r\\n                />\\r\\n              </div>\\r\\n            </section>\\r\\n          </main>\\r\\n        </div>\\r\\n      </div>\\r\\n      <aside class=\\\"botton-section\\\">\\r\\n        <div class=\\\"div\\\">\\r\\n          <div class=\\\"column-5\\\">\\r\\n            <img\\r\\n              loading=\\\"lazy\\\"\\r\\n              src=\\\"\" + ___HTML_LOADER_REPLACEMENT_4___ + \"\\\"\\r\\n              class=\\\"image-about-dark\\\"\\r\\n              alt=\\\"image about dark\\\"\\r\\n            />\\r\\n          </div>\\r\\n          <div class=\\\"column-6\\\">\\r\\n            <div class=\\\"about-content\\\">\\r\\n              <h2 class=\\\"about-title\\\">ABOUT OUR FURNITURE</h2>\\r\\n              <p class=\\\"p-content\\\">\\r\\n                Our multifunctional collection blends design and function to\\r\\n                suit your individual taste. Make each room unique, or pick a\\r\\n                cohesive theme that best express your interests and what\\r\\n                inspires you. Find the furniture pieces you need, from\\r\\n                traditional to contemporary styles or anything in between.\\r\\n                Product specialists are available to help you create your dream\\r\\n                space.\\r\\n              </p>\\r\\n            </div>\\r\\n          </div>\\r\\n          <div class=\\\"column-7\\\">\\r\\n            <img\\r\\n              loading=\\\"lazy\\\"\\r\\n              src=\\\"\" + ___HTML_LOADER_REPLACEMENT_5___ + \"\\\"\\r\\n              class=\\\"image-about-light\\\"\\r\\n              alt=\\\"image about light\\\"\\r\\n            />\\r\\n          </div>\\r\\n        </div>\\r\\n      </aside>\\r\\n    </div>\\r\\n  </body>\\r\\n</html>\\r\\n\";\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (code);\n\n//# sourceURL=webpack://room-homepage-master/./src/template.html?");
+
+/***/ }),
+
+/***/ "./node_modules/html-loader/dist/runtime/getUrl.js":
+/*!*********************************************************!*\
+  !*** ./node_modules/html-loader/dist/runtime/getUrl.js ***!
+  \*********************************************************/
+/***/ ((module) => {
+
+eval("\n\nmodule.exports = function (url, options) {\n  if (!options) {\n    // eslint-disable-next-line no-param-reassign\n    options = {};\n  }\n  if (!url) {\n    return url;\n  }\n\n  // eslint-disable-next-line no-underscore-dangle, no-param-reassign\n  url = String(url.__esModule ? url.default : url);\n  if (options.hash) {\n    // eslint-disable-next-line no-param-reassign\n    url += options.hash;\n  }\n  if (options.maybeNeedQuotes && /[\\t\\n\\f\\r \"'=<>`]/.test(url)) {\n    return \"\\\"\".concat(url, \"\\\"\");\n  }\n  return url;\n};\n\n//# sourceURL=webpack://room-homepage-master/./node_modules/html-loader/dist/runtime/getUrl.js?");
+
+/***/ }),
+
+/***/ "./assets/icon-angle-left.svg":
+/*!************************************!*\
+  !*** ./assets/icon-angle-left.svg ***!
+  \************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/icon-angle-left.svg\";\n\n//# sourceURL=webpack://room-homepage-master/./assets/icon-angle-left.svg?");
+
+/***/ }),
+
+/***/ "./assets/icon-angle-right.svg":
+/*!*************************************!*\
+  !*** ./assets/icon-angle-right.svg ***!
+  \*************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/icon-angle-right.svg\";\n\n//# sourceURL=webpack://room-homepage-master/./assets/icon-angle-right.svg?");
+
+/***/ }),
+
+/***/ "./assets/icon-arrow.svg":
+/*!*******************************!*\
+  !*** ./assets/icon-arrow.svg ***!
+  \*******************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/icon-arrow.svg\";\n\n//# sourceURL=webpack://room-homepage-master/./assets/icon-arrow.svg?");
+
+/***/ }),
+
+/***/ "./assets/image-about-dark.jpg":
+/*!*************************************!*\
+  !*** ./assets/image-about-dark.jpg ***!
+  \*************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/image-about-dark.jpg\";\n\n//# sourceURL=webpack://room-homepage-master/./assets/image-about-dark.jpg?");
+
+/***/ }),
+
+/***/ "./assets/image-about-light.jpg":
+/*!**************************************!*\
+  !*** ./assets/image-about-light.jpg ***!
+  \**************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/image-about-light.jpg\";\n\n//# sourceURL=webpack://room-homepage-master/./assets/image-about-light.jpg?");
+
+/***/ }),
+
+/***/ "./assets/logo.svg":
+/*!*************************!*\
+  !*** ./assets/logo.svg ***!
+  \*************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/logo.svg\";\n\n//# sourceURL=webpack://room-homepage-master/./assets/logo.svg?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && !scriptUrl) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		__webpack_require__.b = document.baseURI || self.location.href;
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"main": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		// no on chunks loaded
+/******/ 		
+/******/ 		// no jsonp function
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/template.html");
+/******/ 	
+/******/ })()
+;
